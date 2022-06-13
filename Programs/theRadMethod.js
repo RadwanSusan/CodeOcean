d851ceefe97ce263e77b773e3ad45fd53552ed98
radwan susan <radwansusan90@gmail.com>
2022-06-13 23:20:44 +0300

    Update theRadMethod.js

--- Programs/theRadMethod.js
+++ Programs/theRadMethod.js
@@ -1,32 +1,34 @@
 // jshint esversion:6
-let theRADMethod = (doc, query) => {
+const theRADMethod = (doc, query) => {
 	doc = doc.toLowerCase();
 	query = query.toLowerCase();
-	let array1 = [];
-	let array2 = [];
-	let str1 = doc.split(" ");
-	let str2 = query.split(" ");
-	let str3 = doc.concat(" ", query).split(" ");
-	let str4 = str3.filter((item, index, array) => array.indexOf(item) === index);
-	for (let i = 0; i < str4.length; i++) {
+	const array1 = [];
+	const array2 = [];
+	const str1 = doc.split(" ");
+	const str2 = query.split(" ");
+	const str3 = doc.concat(" ", query).split(" ");
+	const str4 = str3.filter(
+		(item, index, array) => array.indexOf(item) === index,
+	);
+	for (const element of str4) {
 		let count = 0;
-		for (let j = 0; j < str1.length; j++) {
-			if (str4[i] === str1[j]) {
+		for (const element2 of str1) {
+			if (element === element2) {
 				count++;
 			}
 		}
 		array1.push(count);
 	}
-	for (let i = 0; i < str4.length; i++) {
+	for (const element of str4) {
 		let count = 0;
-		for (let j = 0; j < str2.length; j++) {
-			if (str4[i] === str2[j]) {
+		for (const element2 of str2) {
+			if (element === element2) {
 				count++;
 			}
 		}
 		array2.push(count);
 	}
-	let cosinesim = (A, B) => {
+	const cosinesim = (A, B) => {
 		let dotproduct = 0;
 		let mA = 0;
 		let mB = 0;
@@ -37,32 +39,29 @@ let theRADMethod = (doc, query) => {
 		}
 		mA = Math.sqrt(mA);
 		mB = Math.sqrt(mB);
-		let similarity = dotproduct / (mA * mB);
-		return similarity;
+		return dotproduct / (mA * mB);
 	};
-	let p = cosinesim(array1, array2);
-	return p;
+	return cosinesim(array1, array2);
 };
-let Technology = "Technology is a new way of thinking about the world";
-let Sport = "Sport is very important to the world";
-let Fashion = "Fashion is a new way of thinking about the world";
-let query = "World";
-
-let decide = (Technology, Sport, Fashion, query) => {
-	let p1 = theRADMethod(Technology, query);
-	let p2 = theRADMethod(Sport, query);
-	let p3 = theRADMethod(Fashion, query);
-	let max = Math.max(p1, p2, p3);
+const Technology = "Technology is a new way of thinking about the world";
+const Sport = "Sport is very important to the world";
+const Fashion = "Fashion is a new way of thinking about the world";
+const query = "World";
+const decide = (Technology, Sport, Fashion, query) => {
+	const p1 = theRADMethod(Technology, query);
+	const p2 = theRADMethod(Sport, query);
+	const p3 = theRADMethod(Fashion, query);
+	const max = Math.max(p1, p2, p3);
 	console.log(`Technology (${p1})`);
 	console.log(`Sport (${p2})`);
 	console.log(`Fashion (${p3})`);
-
-	if (max === p1) {
-		return `the document is Technology`;
-	} else if (max === p2) {
-		return `the document is Sport`;
-	} else if (max === p3) {
-		return `the document is Fashion`;
+	switch (max) {
+		case p1:
+			return `the document is Technology`;
+		case p2:
+			return `the document is Sport`;
+		case p3:
+			return `the document is Fashion`;
 	}
 };
 console.log(decide(Technology, Sport, Fashion, query));
